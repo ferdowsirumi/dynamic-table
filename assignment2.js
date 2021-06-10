324ff10efed02425231b9fbf9f2fb2bb4f23d14f
ferdowsi rumi <ferdowsirumi@gmail.com>
2021-06-09 21:45:28 -0400

    added form

--- assignment2.js
+++ assignment2.js
@@ -3,27 +3,34 @@ var ROW_LIMIT = 15;
 var COLUMN_LIMIT = 15;
 
 window.addEventListener('load', drawTable, false);
+
 function redrawTable() {
-    ROW_LIMIT = parseInt(prompt("Enter a ROW Size", "0"));
-    COLUMN_LIMIT = parseInt(prompt("Enter a COLUMN Size", "0"));
-    drawTable();
+    var redrawForm = document.getElementById("redrawForm");
+    var tableRawField = document.getElementById("tableRow");
+    var tableColumnField = document.getElementById("tableColumn")
+    if (redrawForm.checkValidity()) {
+        ROW_LIMIT = isNaN(parseInt(tableRawField.value)) ? 0 : parseInt(tableRawField.value);
+        COLUMN_LIMIT = isNaN(parseInt(tableColumnField.value)) ? 0 : parseInt(tableColumnField.value);
+        drawTable();
+        document.getElementById("validationResult").innerHTML = "";
+        document.getElementById('tablePanel').scrollIntoView();
+    }
+    else {
+        document.getElementById("validationResult").innerHTML = "We need both number of raws and columns to draw the table";
+    }
 }
-
 function drawTable() {
-
     var myTable = "";
-
     for (var i = 1; i <= ROW_LIMIT; i++) {
         myTable += '<tr>';
         for (var j = 1; j <= COLUMN_LIMIT; j++) {
-            myTable += 
-            '<td> ' +
-                    ' <p>' + i + ", " + j + '</p> ' +
-            '</td>';
+            myTable +=
+                '<td> ' +
+                ' <p>' + i + ", " + j + '</p> ' +
+                '</td>';
         }
         myTable += '</tr>'
     }
-    
     document.getElementById("myTable").innerHTML = myTable;
 }
 
